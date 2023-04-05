bf486f43f0a379511c3cbb30e40e1aaa6f18ef77
gfg7 <nakonechnaya040702@gmail.com>
2023-04-06 02:14:34 +0500

    library

---
+++ Mindbox/MindboxLibrary.cs
@@ -0,0 +1,9 @@
+﻿using Mindbox.Interfaces;
+
+namespace Mindbox;
+public class MindboxLibrary
+{
+    public double GetArea(IBaseFigure figure) => figure.GetArea();
+
+    public bool IsTriangleSquare(ITriangle triangle) => triangle.IsSquare();
+}
